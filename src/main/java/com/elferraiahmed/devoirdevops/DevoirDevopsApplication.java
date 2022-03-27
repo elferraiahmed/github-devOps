45c998218f47dd0bd0a455ceaa79d25a7f074df1
elferraiahmed <fabien.callinout@gmail.com>
2022-03-27 16:15:54 +0200

    code

---
+++ src/main/java/com/elferraiahmed/devoirdevops/DevoirDevopsApplication.java
@@ -0,0 +1,29 @@
+package com.elferraiahmed.devoirdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevoirDevopsApplication {
+
+    @GetMapping("/dev_Ops")
+    public String dev_Ops(){
+        return "devoir devOps";
+    }
+
+//    echo "# github-devOps" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/elferraiahmed/github-devOps.git
+//    git push -u origin main
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevoirDevopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elferraiahmed/devoirdevops/DevoirDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elferraiahmed.devoirdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevoirDevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
